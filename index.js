e8936b77098fed0b0f763f91fab11e525617931f
Long Do <doduclong2208@gmail.com>
2020-04-02 09:42:03 +0700

    Set environment

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+console.log(process.env.SESSION_SECRET)
 const express = require('express');
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
@@ -9,10 +11,9 @@ const authMiddleware = require('./middleware/auth.middleware')
 
 const app = express();
 const POST =process.env.POST || 3000;
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieParser("lsahhfsahfsa@$@$"))
+app.use(cookieParser(process.env.SESSION_SECRET))
 
 app.set('view engine','pug');
 app.set('views','./view')
